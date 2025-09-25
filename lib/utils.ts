8348e1aa160ddfea3e687dd86d9c68b76ed45b6a
JahongirHakimjonov <jahongirhakimjonov@gmail.com>
2025-09-25 18:35:19 +0500

    fix

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,9 +1,9 @@
-import { clsx, type ClassValue } from 'clsx';
-import { twMerge } from 'tailwind-merge';
+import {type ClassValue, clsx} from 'clsx';
+import {twMerge} from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+    return twMerge(clsx(inputs));
 }
 
-export const BASE_URL = process.env.BASE_URL;
-export const WS_URL = process.env.WS_URL;
+export const BASE_URL = process.env.BASE_URL || 'https://ai-back.milliytech.uz';
+export const WS_URL = process.env.WS_URL || 'wss://ai-back.milliytech.uz';
