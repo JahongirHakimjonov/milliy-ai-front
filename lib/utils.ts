fd17c886f1aadd567b116a2dea5bb484ceb7e68f
JahongirHakimjonov <jahongirhakimjonov@gmail.com>
2025-09-25 18:31:56 +0500

    fix

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,5 +5,5 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const BASE_URL = process.env.BASE_URL || 'http://localhost:8000';
-export const WS_URL = process.env.WS_URL || 'ws://localhost:8000';
+export const BASE_URL = process.env.BASE_URL;
+export const WS_URL = process.env.WS_URL;
