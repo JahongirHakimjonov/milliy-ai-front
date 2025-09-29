097cc36f0eb26d452271fc835f0b18a2d0493c49
JahongirHakimjonov <jahongirhakimjonov@gmail.com>
2025-09-29 16:22:21 +0500

    fix

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,8 +5,8 @@ export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
 }
 
-// export const BASE_URL = process.env.BASE_URL || 'https://ai-back.milliytech.uz';
-// export const WS_URL = process.env.WS_URL || 'wss://ai-back.milliytech.uz';
+export const BASE_URL = process.env.BASE_URL || 'https://ai-back.milliytech.uz';
+export const WS_URL = process.env.WS_URL || 'wss://ai-back.milliytech.uz';
 
-export const BASE_URL = process.env.BASE_URL || 'http://127.0.0.1:8010';
-export const WS_URL = process.env.WS_URL || 'ws://127.0.0.1:8010';
+// export const BASE_URL = process.env.BASE_URL || 'http://127.0.0.1:8010';
+// export const WS_URL = process.env.WS_URL || 'ws://127.0.0.1:8010';
